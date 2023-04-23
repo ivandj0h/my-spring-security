f9b23adc468625a9572a4e25d248fc76053c495b
ivandjoh <ivandi.djoh@gmail.com>
2023-04-23 17:24:59 +0800

    add apiSecurityFilterChain

--- src/main/java/com/ivandjoh/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/ivandjoh/springsecurity/config/SecurityConfig.java
@@ -2,16 +2,34 @@ package com.ivandjoh.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    // /api/** authenticated / HttpBasic
     @Bean
+    SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth-> {
+                    auth.requestMatchers("/api/**").authenticated();
+                })
+                .sessionManagement(session-> session.sessionCreationPolicy(STATELESS))
+                .httpBasic(withDefaults())
+                .build();
+    }
+
+    // /h2-console/** permitAll / no csrf / no frameOptions
+    @Bean
+    @Order(1)
     SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth-> {
@@ -21,4 +39,5 @@ public class SecurityConfig {
                 .headers(headers-> headers.frameOptions().disable())
                 .build();
     }
+
 }
